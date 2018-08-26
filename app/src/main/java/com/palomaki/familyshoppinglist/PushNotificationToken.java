4b472908cab1b583ba5d4e72ccc90e02011278b4
Jukka A Palomaki <palomakijukka@gmail.com>
2018-08-26 16:50:55 +0300

    Renamed package. Added some firebase push notification stuff.

---
+++ app/src/main/java/com/palomaki/familyshoppinglist/PushNotificationToken.java
@@ -0,0 +1,22 @@
+package com.palomaki.familyshoppinglist;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class PushNotificationToken extends FirebaseInstanceIdService {
+    private static final String TAG = "PushNotificationToken";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        //sendRegistrationToServer(refreshedToken);
+    }
+}
