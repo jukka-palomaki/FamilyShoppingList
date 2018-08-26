6a649cde229e8d43626b6e06ebf7e5c5ad10787f
Jukka A Palomaki <palomakijukka@gmail.com>
2018-08-26 18:08:51 +0300

    Quite close for a working push notification...

---
+++ app/src/main/java/com/palomaki/familyshoppinglist/NotificationSettings.kt
@@ -0,0 +1,9 @@
+package com.palomaki.familyshoppinglist
+
+object NotificationSettings {
+
+    var SenderId = "<Your project number>"
+    var HubName = "<Your HubName>"
+    var HubListenConnectionString = "<Enter your DefaultListenSharedAccessSignature connection string>"
+}
+
