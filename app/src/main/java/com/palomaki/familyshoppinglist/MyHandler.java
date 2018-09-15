e461ce88d1e333aa5f4178426de029ba2415619b
Jukka A Palomaki <palomakijukka@gmail.com>
2018-09-15 14:07:11 +0300

    Tried firebase and google cloud messaging solutions. Not yet working. Registration works anyway but receiving of messages does not work sent from azure or from other phone

--- app/src/main/java/com/palomaki/familyshoppinglist/MyHandler.java
+++ app/src/main/java/com/palomaki/familyshoppinglist/MyHandler.java
@@ -8,16 +8,44 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
+import android.util.Base64;
+import android.view.View;
+import android.widget.EditText;
+
+import com.microsoft.windowsazure.messaging.NotificationHub;
 import com.microsoft.windowsazure.notifications.NotificationsHandler;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
 
 public class MyHandler extends NotificationsHandler {
     public static final int NOTIFICATION_ID = 1;
     private NotificationManager mNotificationManager;
-    NotificationCompat.Builder builder;
+    NotificationCompat.Builder mBuilder;
     Context ctx;
+
+    private static String HubEndpoint = null;
+    private static String HubSasKeyName = null;
+    private static String HubSasKeyValue = null;
+
+
+    private NotificationHub hub;
 
     public static ToDoActivity todoActivity;
     public static Boolean isVisible = false;
+
+    public MyHandler(Context ctx) {
+        this.ctx = ctx;
+    }
 
     @Override
     public void onReceive(Context context, Bundle bundle) {
@@ -27,9 +55,11 @@ public class MyHandler extends NotificationsHandler {
         if (isVisible) {
             todoActivity.ToastNotify(nhMessage);
         }
+        super.onReceive(context, bundle);
     }
 
-    private void sendNotification(String msg) {
+
+    public void sendNotification(String msg) {
 
         Intent intent = new Intent(ctx, ToDoActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -41,7 +71,7 @@ public class MyHandler extends NotificationsHandler {
                 intent, PendingIntent.FLAG_ONE_SHOT);
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder mBuilder =
+        mBuilder =
                 new NotificationCompat.Builder(ctx)
                         .setSmallIcon(R.drawable.ic_launcher)
                         .setContentTitle("Notification Hub Demo")
@@ -52,5 +82,156 @@ public class MyHandler extends NotificationsHandler {
 
         mBuilder.setContentIntent(contentIntent);
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+
+        todoActivity.ToastNotify(msg);
     }
+
+
+    private static String generateSasToken(String uri) {
+
+        String targetUri;
+        String token = null;
+        try {
+            targetUri = URLEncoder
+                    .encode(uri.toString().toLowerCase(), "UTF-8")
+                    .toLowerCase();
+
+            long expiresOnDate = System.currentTimeMillis();
+            int expiresInMins = 60; // 1 hour
+            expiresOnDate += expiresInMins * 60 * 1000;
+            long expires = expiresOnDate / 1000;
+            String toSign = targetUri + "\n" + expires;
+
+            // Get an hmac_sha1 key from the raw key bytes
+            byte[] keyBytes = HubSasKeyValue.getBytes("UTF-8");
+            SecretKeySpec signingKey = new SecretKeySpec(keyBytes, "HmacSHA256");
+
+            // Get an hmac_sha1 Mac instance and initialize with the signing key
+            Mac mac = Mac.getInstance("HmacSHA256");
+            mac.init(signingKey);
+
+            // Compute the hmac on input data bytes
+            byte[] rawHmac = mac.doFinal(toSign.getBytes("UTF-8"));
+
+            // Using android.util.Base64 for Android Studio instead of
+            // Apache commons codec
+            String signature = URLEncoder.encode(
+                    Base64.encodeToString(rawHmac, Base64.NO_WRAP).toString(), "UTF-8");
+
+            // Construct authorization string
+            token = "SharedAccessSignature sr=" + targetUri + "&sig="
+                    + signature + "&se=" + expires + "&skn=" + HubSasKeyName;
+        } catch (Exception e) {
+            if (isVisible) {
+                todoActivity.ToastNotify("Exception Generating SaS : " + e.getMessage().toString());
+            }
+        }
+
+        return token;
+    }
+
+
+    /**
+     * Send Notification button click handler. This method parses the
+     * DefaultFullSharedAccess connection string and generates a SaS token. The
+     * token is added to the Authorization header on the POST request to the
+     * notification hub. The text in the editTextNotificationMessage control
+     * is added as the JSON body for the request to add a GCM message to the hub.
+     *
+     * @param v
+     */
+    public static void sendNotificationButtonOnClick(View v) {
+        EditText notificationText = (EditText) todoActivity.findViewById(R.id.editTextNotificationMessage);
+        final String json = "{\"data\":{\"message\":\"" + notificationText.getText().toString() + "\"}}";
+
+        new Thread()
+        {
+            public void run()
+            {
+                try
+                {
+                    // Based on reference documentation...
+                    // http://msdn.microsoft.com/library/azure/dn223273.aspx
+                    parseConnectionString();
+                    URL url = new URL(HubEndpoint + NotificationSettings.INSTANCE.getHubName() +
+                            "/messages/?api-version=2015-01");
+
+                    HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+
+                    try {
+                        // POST request
+                        urlConnection.setDoOutput(true);
+
+                        // Authenticate the POST request with the SaS token
+                        urlConnection.setRequestProperty("Authorization",
+                                generateSasToken(url.toString()));
+
+                        // Notification format should be GCM
+                        urlConnection.setRequestProperty("ServiceBusNotification-Format", "gcm");
+
+                        // Include any tags
+                        // Example below targets 3 specific tags
+                        // Refer to : https://azure.microsoft.com/en-us/documentation/articles/notification-hubs-routing-tag-expressions/
+                        // urlConnection.setRequestProperty("ServiceBusNotification-Tags",
+                        //        "tag1 || tag2 || tag3");
+
+                        // Send notification message
+                        urlConnection.setFixedLengthStreamingMode(json.length());
+                        OutputStream bodyStream = new BufferedOutputStream(urlConnection.getOutputStream());
+                        bodyStream.write(json.getBytes());
+                        bodyStream.close();
+
+                        // Get reponse
+                        urlConnection.connect();
+                        int responseCode = urlConnection.getResponseCode();
+                        if ((responseCode != 200) && (responseCode != 201)) {
+                            BufferedReader br = new BufferedReader(new InputStreamReader((urlConnection.getErrorStream())));
+                            String line;
+                            StringBuilder builder = new StringBuilder("Send Notification returned " +
+                                    responseCode + " : ")  ;
+                            while ((line = br.readLine()) != null) {
+                                builder.append(line);
+                            }
+
+                            todoActivity.ToastNotify(builder.toString());
+                        }
+                    } finally {
+                        urlConnection.disconnect();
+                    }
+                }
+                catch(Exception e)
+                {
+                    if (isVisible) {
+                        todoActivity.ToastNotify("Exception Sending Notification : " + e.getMessage().toString());
+                    }
+                }
+            }
+        }.start();
+    }
+
+    /**
+     * Example code from http://msdn.microsoft.com/library/azure/dn495627.aspx
+     * to parse the connection string so a SaS authentication token can be
+     * constructed.
+     *
+     */
+    private static void parseConnectionString()
+    {
+        String connectionString = NotificationSettings.INSTANCE.getHubFullAccess();
+        String[] parts = connectionString.split(";");
+        if (parts.length != 3)
+            throw new RuntimeException("Error parsing connection string: "
+                    + connectionString);
+
+        for (int i = 0; i < parts.length; i++) {
+            if (parts[i].startsWith("Endpoint")) {
+                HubEndpoint = "https" + parts[i].substring(11);
+            } else if (parts[i].startsWith("SharedAccessKeyName")) {
+                HubSasKeyName = parts[i].substring(20);
+            } else if (parts[i].startsWith("SharedAccessKey")) {
+                HubSasKeyValue = parts[i].substring(16);
+            }
+        }
+    }
+
 }
