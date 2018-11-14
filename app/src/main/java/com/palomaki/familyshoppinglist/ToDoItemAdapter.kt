7e9aae7c35b954575e1a9febe25dfdd4eeb7a89a
Jukka A Palomaki <palomakijukka@gmail.com>
2018-11-14 14:38:25 +0200

    Action key works now in keyboard while editing shopping item

--- app/src/main/java/com/palomaki/familyshoppinglist/ToDoItemAdapter.kt
+++ app/src/main/java/com/palomaki/familyshoppinglist/ToDoItemAdapter.kt
@@ -3,6 +3,7 @@ package com.palomaki.familyshoppinglist
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.text.Editable
 import android.view.View
 import android.view.ViewGroup
@@ -30,7 +31,7 @@ class ToDoItemAdapter(
     /**
      * Returns the view for a specific item on the list
      */
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View? {
         var row = convertView
 
         val currentItem = getItem(position)
@@ -38,11 +39,14 @@ class ToDoItemAdapter(
         if (row == null) {
             val inflater = (mContext as Activity).layoutInflater
             row = inflater.inflate(mLayoutResourceId, parent, false)
+            if (row == null) {
+                return null //should never happen
+            }
         }
 
-        row!!.tag = currentItem
-        val checkBox = row.findViewById(R.id.checkToDoItem) as CheckBox
-        checkBox.text = currentItem!!.text.trim()
+        row.tag = currentItem
+        val checkBox = row.findViewById<CheckBox>(R.id.checkToDoItem)
+        checkBox.text = currentItem.text.trim()
         checkBox.isChecked = false
         checkBox.isEnabled = true
 
@@ -58,36 +62,47 @@ class ToDoItemAdapter(
 
             val alert = AlertDialog.Builder(mContext)
             val editTextUpdatedText = EditText(context)
+            val dialog = alert.create()
+
             editTextUpdatedText.imeOptions = EditorInfo.IME_ACTION_DONE
             editTextUpdatedText.setSingleLine(true)
 
-            // Builder
-            with (alert) {
-                editTextUpdatedText.text = Editable.Factory.getInstance().newEditable(checkBox.text.toString())
+            fun doEditActions() {
+                val newValue = editTextUpdatedText.text.toString().trim()
 
-
-                setPositiveButton("Update") {
-                    dialog, whichButton ->
-                    dialog.dismiss()
-                    val newValue = editTextUpdatedText.text.toString().trim()
-
-                    if (newValue.isEmpty()) { // when updated to empty text that is same as delete task
-                        currentItem.isComplete = true
-                        checkBox.isChecked = true
-                    }
-                    checkBox.text = newValue
-                    currentItem.text = newValue
-                    updateData(currentItem)
+                if (newValue.isEmpty()) { // when updated to empty text that is same as delete task
+                    currentItem.isComplete = true
+                    checkBox.isChecked = true
                 }
+                checkBox.text = newValue
+                currentItem.text = newValue
+                updateData(currentItem)
+                dialog.dismiss()
+            }
 
-                setNegativeButton("Keep original") {
-                    dialog, whichButton ->
-                    dialog.dismiss()
+            editTextUpdatedText.setOnEditorActionListener { v, actionId, event ->
+                return@setOnEditorActionListener when (actionId) {
+                    EditorInfo.IME_ACTION_DONE -> {
+                        doEditActions()
+                        true
+                    }
+                    else -> false
                 }
             }
 
-            // Dialog
-            val dialog = alert.create()
+            with (alert) {
+                editTextUpdatedText.text = Editable.Factory.getInstance().newEditable(checkBox.text.toString())
+
+                setPositiveButton("Update") {
+                    dialog, whichButton ->
+                        doEditActions()
+                }
+
+                setNegativeButton("Keep original") {
+                    dialog, whichButton -> dialog.dismiss()
+                }
+            }
+
             dialog.setView(editTextUpdatedText)
             dialog.show()
 
